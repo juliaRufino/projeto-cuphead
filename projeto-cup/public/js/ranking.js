a82a65db85b94847c8a33fc86721fb334748bf8a
Julia Rufino <julia.rufino@sptech.school>
2023-05-16 00:12:01 -0300

    Arrumando tabela e adicionando o vetor para pegar os dados dos radio button

---
+++ projeto-cup/public/js/ranking.js
@@ -0,0 +1,210 @@
+function iniciarRanking() {
+    document.getElementById("conteudoPrincipal").style.display = "none"
+    document.getElementById("rankingChefe1").style.display = "flex"
+}
+
+function cancelarRanking() {
+    document.getElementById("conteudoPrincipal").style.display = "flex"
+    document.getElementById("rankingChefe1").style.display = "none"
+}
+
+function voltarRanking() {
+    document.getElementById("rankingChefe1").style.display = "flex"
+    document.getElementById("rankingChefe2").style.display = "none"
+}
+
+function voltarRanking1() {
+    document.getElementById("rankingChefe2").style.display = "flex"
+    document.getElementById("rankingChefe3").style.display = "none"
+}
+
+function proximoChefe() {
+    document.getElementById("rankingChefe2").style.display = "flex"
+    document.getElementById("rankingChefe1").style.display = "none"
+}
+
+function proximoChefe1() {
+    document.getElementById("rankingChefe3").style.display = "flex"
+    document.getElementById("rankingChefe2").style.display = "none"
+}
+
+function voltarRanking2() {
+    document.getElementById("rankingChefe3").style.display = "flex"
+    document.getElementById("rankingChefe4").style.display = "none"
+}
+
+function proximoChefe2() {
+    document.getElementById("rankingChefe4").style.display = "flex"
+    document.getElementById("rankingChefe3").style.display = "none"
+}
+
+function voltarRanking3() {
+    document.getElementById("rankingChefe3").style.display = "flex"
+    document.getElementById("rankingChefe4").style.display = "none"
+}
+
+function proximoChefe3() {
+    document.getElementById("rankingChefe5").style.display = "flex"
+    document.getElementById("rankingChefe4").style.display = "none"
+}
+
+function voltarRanking4() {
+    document.getElementById("rankingChefe4").style.display = "flex"
+    document.getElementById("rankingChefe5").style.display = "none"
+}
+
+function proximoChefe4() {
+    document.getElementById("rankingChefe6").style.display = "flex"
+    document.getElementById("rankingChefe5").style.display = "none"
+}
+
+function voltarRanking5() {
+    document.getElementById("rankingChefe5").style.display = "flex"
+    document.getElementById("rankingChefe6").style.display = "none"
+}
+
+function proximoChefe5() {
+    document.getElementById("rankingChefe7").style.display = "flex"
+    document.getElementById("rankingChefe6").style.display = "none"
+}
+
+function voltarRanking6() {
+    document.getElementById("rankingChefe6").style.display = "flex"
+    document.getElementById("rankingChefe7").style.display = "none"
+}
+
+function proximoChefe6() {
+    document.getElementById("rankingChefe8").style.display = "flex"
+    document.getElementById("rankingChefe7").style.display = "none"
+}
+
+function voltarRanking7() {
+    document.getElementById("rankingChefe7").style.display = "flex"
+    document.getElementById("rankingChefe8").style.display = "none"
+}
+
+function proximoChefe7() {
+    document.getElementById("rankingChefe9").style.display = "flex"
+    document.getElementById("rankingChefe8").style.display = "none"
+}
+
+function voltarRanking8() {
+    document.getElementById("rankingChefe8").style.display = "flex"
+    document.getElementById("rankingChefe9").style.display = "none"
+}
+
+function proximoChefe8() {
+    document.getElementById("rankingChefe10").style.display = "flex"
+    document.getElementById("rankingChefe9").style.display = "none"
+}
+
+function voltarRanking9() {
+    document.getElementById("rankingChefe9").style.display = "flex"
+    document.getElementById("rankingChefe10").style.display = "none"
+}
+
+function proximoChefe9() {
+    document.getElementById("rankingChefe11").style.display = "flex"
+    document.getElementById("rankingChefe10").style.display = "none"
+}
+
+function voltarRanking10() {
+    document.getElementById("rankingChefe10").style.display = "flex"
+    document.getElementById("rankingChefe11").style.display = "none"
+}
+
+function proximoChefe10() {
+    document.getElementById("rankingChefe12").style.display = "flex"
+    document.getElementById("rankingChefe11").style.display = "none"
+}
+
+function voltarRanking11() {
+    document.getElementById("rankingChefe11").style.display = "flex"
+    document.getElementById("rankingChefe12").style.display = "none"
+}
+
+function proximoChefe11() {
+    document.getElementById("rankingChefe13").style.display = "flex"
+    document.getElementById("rankingChefe12").style.display = "none"
+}
+
+function voltarRanking12() {
+    document.getElementById("rankingChefe12").style.display = "flex"
+    document.getElementById("rankingChefe13").style.display = "none"
+}
+
+function proximoChefe12() {
+    document.getElementById("rankingChefe14").style.display = "flex"
+    document.getElementById("rankingChefe13").style.display = "none"
+}
+
+function voltarRanking13() {
+    document.getElementById("rankingChefe13").style.display = "flex"
+    document.getElementById("rankingChefe14").style.display = "none"
+}
+
+function proximoChefe13() {
+    document.getElementById("rankingChefe15").style.display = "flex"
+    document.getElementById("rankingChefe14").style.display = "none"
+}
+
+function voltarRanking14() {
+    document.getElementById("rankingChefe14").style.display = "flex"
+    document.getElementById("rankingChefe15").style.display = "none"
+}
+
+function proximoChefe14() {
+    document.getElementById("rankingChefe16").style.display = "flex"
+    document.getElementById("rankingChefe15").style.display = "none"
+}
+
+function voltarRanking15() {
+    document.getElementById("rankingChefe15").style.display = "flex"
+    document.getElementById("rankingChefe16").style.display = "none"
+}
+
+function proximoChefe15() {
+    document.getElementById("rankingChefe17").style.display = "flex"
+    document.getElementById("rankingChefe16").style.display = "none"
+}
+
+function voltarRanking16() {
+    document.getElementById("rankingChefe16").style.display = "flex"
+    document.getElementById("rankingChefe17").style.display = "none"
+}
+
+function proximoChefe16() {
+    document.getElementById("rankingChefe18").style.display = "flex"
+    document.getElementById("rankingChefe17").style.display = "none"
+}
+
+function voltarRanking17() {
+    document.getElementById("rankingChefe17").style.display = "flex"
+    document.getElementById("rankingChefe18").style.display = "none"
+}
+
+function proximoChefe17() {
+    document.getElementById("rankingChefe19").style.display = "flex"
+    document.getElementById("rankingChefe18").style.display = "none"
+}
+
+function voltarRanking18() {
+    document.getElementById("rankingChefe18").style.display = "flex"
+    document.getElementById("rankingChefe19").style.display = "none"
+}
+
+var escolhasDificuldade = []
+function finalizarRanking() {
+    for (var i = 1; i <= 19; i++) {
+        escolhas = document.querySelector(`input[name="dificuldadeChefe${i}"]:checked`).value;
+        escolhasDificuldade.push(escolhas)
+    }
+
+    document.getElementById("baseTelaTabelaRanking").style.display = "flex"
+    document.getElementById("rankingChefe19").style.display = "none"
+}
+
+function voltarParaRanking() {
+    document.getElementById("rankingChefe19").style.display = "flex"
+    document.getElementById("baseTelaTabelaRanking").style.display = "none"
+}
